d7ac1e2af6238bd9882ee635ab4f7382beab886f
AhmadM <Ahmad.hmazaal@gmail.com>
2022-06-04 16:11:50 +0300

    feat: add notifications for WatchOS

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -8,5 +8,9 @@ struct LandmarksApp: App {
             ContentView()
                 .environmentObject(modelData);
         }
+        
+        #if os(watchOS)
+        WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+        #endif
     }
 }
